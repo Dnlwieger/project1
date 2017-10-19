f1058feff8c66d30ea410667cb13077cec282025
Daniel <dnlwieger@gmail.com>
2017-10-19 16:02:42 -0400

    Test CORS fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,107 @@
 
-
 var brewAPI = "19168d0b6f6ee2f248cb255e94cf89bc";
+var geoAPI = "AIzaSyAUDOon4WO3ZCaMTAeTtmIXVH_6_I9gi_M"
+// GOOGLE API Geo Locator example URl:
+//https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
 
-// $("#button").on("click", function()){
-// 	searchVal = "#info";
-// }
+// Brewery DB API Url examples:
+// https://api.brewerydb.com/v2/breweries?/locations/locality&key=19168d0b6f6ee2f248cb255e94cf89bc
+// https://api.brewerydb.com/v2/search?type=brewery&q=North%20Carolina&key=19168d0b6f6ee2f248cb255e94cf89bc
 
-var searchResults = "";
-
-
-
- 
-searchVal = "Michigan";
-
-var baseUrl = "https://api.brewerydb.com/v2";
-			var locale = "/?q=/locations/postalCode";
-			var search = "/search?q="+ searchVal +"&withLocations=y";
+searchVal = "Durham";
+beerSearchVal = "Two Hearted";
+geoURL = "https://maps.googleapis.com/maps/api/geocode/json?address="+ searchVal +"&key=AIzaSyAUDOon4WO3ZCaMTAeTtmIXVH_6_I9gi_M";
+var baseUrl = "https://api.brewerydb.com/v2/";
+            var locationSearch = "/locations/locality"
+			var locale = "&withLocations";
+			var search = "/search?type=beer&q="+ beerSearchVal;
+			var newSearch = "search?type=brewery&q=" + searchVal;
 			var beerName = ""
-			var type = "&type=beer";
+			var type = "type=breweries";
 			var key = "&key=19168d0b6f6ee2f248cb255e94cf89bc";
 			var beer = /beer/;
-			var brew = "&withBreweries=y";
+			var brew = "breweries?";
 			var limit = "&limit=5"
 			var geo = "/search/geo/point?lat=35.772096&lng=-78.638614"
 			// var id = response.data[0].id;
-			var fullUrl = baseUrl + search + brew + limit + key;
+			var fullUrl = baseUrl + search + key;
+			newURL = "https://api.brewerydb.com/v2/breweries?/locations/locality&key=19168d0b6f6ee2f248cb255e94cf89bc"
 			// var fullUrl = baseUrl + search + beerName + type + key;
 var queryURL = fullUrl;
 console.log(fullUrl);
 
 $(document).ready(function() {
     console.log( "ready!" );
-
+//  takes a CITY NAME and returns latitude and longitude for the brewery API
 $.ajax({
-      url: queryURL,
+      url: geoURL,
       method: 'GET'
     })
 		.done(function(response) {
+         
+         console.log(response.results[0].geometry.location.lat);
+         var searchLat = response.results[0].geometry.location.lat;
+         console.log(response.results[0].geometry.location.lng);
+         var searchLong = response.results[0].geometry.location.lng;
+
+searchBrewsInTown(searchLat, searchLong); 
+})
+         function searchBrewsInTown(searchLat, searchLong) {
+        var authKey = "6bf963895d0729291380bc463329fa0e";
+        //var queryURL = "http://api.brewerydb.com/v2/locations?locality=cary&key=6bf963895d0729291380bc463329fa0e";
+        var queryURL2 = "http://api.brewerydb.com/v2/search/geo/point?lat=" + searchLat + "&lng=" + searchLong + "&key=" + authKey;
+// 
+        $.ajax({
+            url: queryURL2,
+            method: "GET"
+        })
+        .done(function(results) {
+            
+            var brewsResults = results.data;
+            for (var i = 0; i < brewsResults.length; i++) {
+            	console.log(results.data[i].brewery.name);
+            	console.log(results.data[i].streetAddress);
+            	console.log(results.data[i].locality);
+            	console.log(results.data[i].phone);            	
+            	console.log(results.data[i].website);
+            	console.log(results.data[i].latitude);
+            	console.log(results.data[i].longitude);
+            } 
+        })
+    }
+ })
+
+var beerSearchVal = beerSearchVal.split(' ').join('_');
+           $.ajax({
+      url: queryURL,
+      method: 'GET'
+    })
+        .done(function(response) {
          console.log(response);
-      
+         for (var i = 0; i < response.data.length; i++) {
+                console.log(response.data[i].name);
+                console.log(response.data[i].style.name);
+                console.log(response.data[i].description);
+                console.log(response.data[i].abv);
+
+            } 
+
+            var getBeerName = $('<h3>').html(response.data[0].name);
+            $("#beerDescription").append(getBeerName);
+            var getBeerStyle = $('<h4>').html(response.data[0].style.name);
+              $("#beerDescription").append(getBeerStyle);
+            var getBeerDescription = $('<p>').html(response.data[0].description);
+              $("#beerDescription").append(getBeerDescription);
+              var abvLabel = $('<h4>').html("ABV: "+ response.data[0].abv);
+              $("#beerAbv").html(abvLabel);
+              
+ 
+})
+
+        $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('#modal1').modal();
+  });
        // .locations[i].locality
       // $.each(response.data[i].locations.locality;);
       // console.log(citySearch);
@@ -49,24 +112,23 @@ $.ajax({
 //     console.log(belaMode);
 // }
 
-			      var dataArray = response.data;
-			for (var i = 0; i < dataArray.length; i++){
-			          	  for (var j = 0; j < dataArray[i].locations.length; j++) {
+			//       var dataArray = response.data;
+			// for (var i = 0; i < dataArray.length; i++){
+			//           	  for (var j = 0; j < dataArray[i].locations.length; j++) {
 
-			          	 		var citySearch = response.data[i].locations[j].locality;
-			          	 		if (citySearch != 'undefined') {
-			          			   j++; }
-			          	 				else {
-			          	 				$("#listArea").append(citySearch+"<br>");
-			          					 }				
-			          	  }
-			}  	 	
-        })
-          
+			//           	 		var citySearch = response.data[i].locations[j].locality;
+			//           	 		if (citySearch != 'undefined') {
+			//           			   j++; }
+			//           	 				else {
+			//           	 				$("#listArea").append(citySearch+"<br>");
+			//           					 }				
+			//           	  }
+			// }  	 	
+       
           	// $("#listArea").append(dataArray[i].abv+"<br>");
           
             
-    })
+   
 
 
 
---
+++ assets/js/search.js
@@ -0,0 +1,178 @@
+var map, infoWindow;
+    function initMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 6
+      });
+      infoWindow = new google.maps.InfoWindow;
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Location found.');
+          infoWindow.open(map);
+          map.setCenter(pos);
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+    };
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+                            'Error: The Geolocation service failed.' :
+                            'Error: Your browser doesn\'t support geolocation.');
+      infoWindow.open(map);
+    };
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+    function initAutocomplete() {
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -33.8688, lng: 151.2195},
+        zoom: 13,
+        mapTypeId: 'roadmap'
+      });
+    // Create the search box and link it to the UI element.
+    //<<< Need to link search term (city) to breweryDB >>>
+    
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+        if (places.length == 0) {
+          return;
+        }
+        // Clear out the old markers.
+        markers.forEach(function(marker) {
+          marker.setMap(null);
+        });
+        markers = [];
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+            places.forEach(function(place) {
+              if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+              }
+              var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+              };
+              // Create a marker for each place.
+              markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+              }));
+              if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+              } else {
+                bounds.extend(place.geometry.location);
+              }
+            });
+            map.fitBounds(bounds);
+        });
+    };
+
+
+
+    // On click function for Search button
+ $("#runSearch").on("click", function(){
+  searchVal = $("#searchCity").val().trim();
+
+    //Empties the search results section
+$("#tbodyid").empty();
+
+ 
+  geoURL = "https://maps.googleapis.com/maps/api/geocode/json?address="+ searchVal +"&key=AIzaSyAUDOon4WO3ZCaMTAeTtmIXVH_6_I9gi_M";
+  console.log(searchVal);
+
+  $(document).ready(function() {
+    console.log( "ready!" );
+//  takes a CITY NAME and returns latitude and longitude for the brewery API
+$.ajax({
+      url: geoURL,
+      method: 'GET'
+    })
+    .done(function(response) {
+         console.log(response);
+         console.log(response.results[0].geometry.location.lat);
+         console.log(response.results[0].geometry.location.lng);
+         var searchLat = response.results[0].geometry.location.lat;
+         var searchLong = response.results[0].geometry.location.lng;
+
+searchBrewsInTown(searchLat, searchLong); 
+})
+         function searchBrewsInTown(searchLat, searchLong) {
+        var authKey = "6bf963895d0729291380bc463329fa0e";
+        //var queryURL = "http://api.brewerydb.com/v2/locations?locality=cary&key=6bf963895d0729291380bc463329fa0e";
+        var queryURL2 = "http://api.brewerydb.com/v2/search/geo/point?lat=" + searchLat + "&lng=" + searchLong + "&key=" + authKey;
+// 
+        $.ajax({
+            url: queryURL2,
+            method: "GET"
+        })
+        .done(function(results) {
+            console.log(results);
+            var brewsResults = results.data;
+            for (var i = 0; i < brewsResults.length; i++) {
+                
+              console.log(results.data[i].brewery.name);      
+              console.log(results.data[i].streetAddress);
+              console.log(results.data[i].locality);
+              console.log(results.data[i].phone)        
+              console.log(results.data[i].website);
+              console.log(results.data[i].latitude);
+              console.log(results.data[i].longitude);
+
+  //Code to append results
+                var tr = $('<tr>');
+                var tdName = $('<td>').append(results.data[i].brewery.name);
+                var tdAddress = $('<td>').append(results.data[i].streetAddress);
+                var tdPhone = $('<td>').append(results.data[i].phone); 
+                var tdWebsite = $('<td>').append("<a href='" + results.data[i].website + "'>" + results.data[i].website + "</a>");
+    
+                tr.append(tdName, tdAddress, tdPhone, tdWebsite );
+                $('tbody').append(tr);
+                
+            } 
+        })
+
+
+    }
+ 
+
+ })
+
+})
+
+  //This button clears the results section
+  $('#clearAll').on('click', function () {
+    $('#tbodyid').empty();
+      console.log("All Clear!");
+      
+      });
